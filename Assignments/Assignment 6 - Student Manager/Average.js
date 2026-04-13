1830b563d4f98eaa371cbe17381c337d764f76a1
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-13 18:41:24 +0530

    added js file

---
+++ Assignments/Assignment 6 - Student Manager/Average.js
@@ -0,0 +1,31 @@
+
+let students = [
+  {
+    name: "Bakkesh",
+    marks: [80, 75, 90]
+  },
+  {
+    name: "Rancho",
+    marks: [85, 70, 88]
+  },
+  {
+    name: "Chatur",
+    marks: [60, 72, 68]
+  }
+];
+
+
+students.forEach(student => {
+  let total = 0;
+
+  student.marks.forEach(mark => {
+    total += mark;
+  });
+
+
+  let average = total / student.marks.length;
+
+  console.log("Student:", student.name);
+  console.log("Average Marks:", average);
+  console.log("----------------------");
+});
